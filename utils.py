5ce6186cac256569d427d7a954856462531350a9
HoAnhKhoaVN <traincolab2131999v1@gmail.com>
2023-06-20 14:49:51 +0700

    Add code visualize image

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
 from typing import Text
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['figure.figsize'] = (12,12)
+mpl.rcParams['axes.grid'] = False
+
 
 
 def load_img(
@@ -31,3 +37,32 @@ def load_img(
 
     img = img[tf.newaxis, :]
     return img
+
+def show_img(
+    image : tf.Tensor,
+    title: Text = None
+)-> None:
+    """_summary_
+
+    Args:
+        image (tf.Tensor): _description_
+        title (Text, optional): _description_. Defaults to None.
+    """
+    if len(image.shape) > 3: # Có nhiều hơn 3 chiều
+        image = tf.squeeze(input=image, axis= 0)
+
+    assert len(image.shape) == 3, f"#Lỗi: Ảnh có {len(image.shape)} chiều!!!"
+
+    plt.imshow(X = image)
+
+    if title:
+        plt.title(label= title)
+
+
+if __name__ == "__main__":
+    IMG_PATH = "content_image.jpg"
+    content_image = load_img(path_to_img= IMG_PATH)
+    show_img(
+        image= content_image,
+        title= "Content Image"
+    )
