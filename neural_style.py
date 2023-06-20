9441d8e9edbf30b76784f5a977c44163b9661241
HoAnhKhoaVN <traincolab2131999v1@gmail.com>
2023-06-20 14:34:52 +0700

    Add loss for neural style transfer

--- neural_style.py
+++ neural_style.py
@@ -1,5 +1,4 @@
 from keras.applications.vgg16 import preprocess_input
-from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras import backend as K
 from scipy.optimize import fmin_l_bfgs_b # Limited-memory Broyden-Fletcher-Goldfarb-Shanno
@@ -7,6 +6,45 @@ from typing import Dict, Text, List, Any
 import numpy as np
 import cv2
 import os
+import tensorflow as tf
+
+
+
+def load_img(
+    path_to_img: Text,
+)-> tf.Tensor:
+    """_summary_
+
+    Args:
+        path_to_img (Text): _description_
+
+    Returns:
+        Tensor: _description_
+    """
+    max_dim = 512
+    img = tf.io.read_file(filename = path_to_img)
+    img = tf.image.decode_image(contents= img, channels=3, dtype= tf.float32)
+
+    shape = tf.cast(tf.shape(input= img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+    print(f"Shape : {shape} - Long_dim : {long_dim} - scale : {scale}")
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(
+        images= img,
+        size = new_shape
+    )
+
+    img = img[tf.newaxis, :]
+    return img
+
+
+
+
+
+    
 
 
 class NeuralStyle:
@@ -56,8 +94,108 @@ class NeuralStyle:
 
         # Xây dựng từ điển ánh xạ tên của mỗi lớp với trọng số bên trong. Để mình sẽ gom nhóm
         # Hay bỏ các lớp theo mong muốn.
-        layer_map = {l.name: l.output for l in self.model.layers}
+        layer_map :Dict = {l.name: l.output for l in self.model.layers}
 
+        # Trích xuất các đặc trưng tư các lớp nội dung
+        content_features : tf.Tensor = layer_map[self.S['content_layer']]
+
+        # Trích xuất các kích hoạt từ phong cách ảnh (chỉ mục 0)
+        style_features : tf.Tensor = content_features[0, :, :, :] # Tại sao 0 là style ?????
+
+        # Trích xuất các kích hoạt từ ảnh đầu ra (chỉ mục 2)
+        output_features : tf.Tensor = content_features[2, :, :, :] # Tại sao chỉ mục 2 là nội dung.
+
+        # Tính toán hàm lỗi cho các đặc trưng tái tạo nội dung
+        content_loss = self.feauture_reconstruction(
+            style_features,
+            output_features
+        )
+
+        content_loss *= self.S['content_weight']
+
+        # Tính lỗi phong cách
+        # Khởi tạo biến lỗi phong cách và trọng số cho mỗi lớp phong cách được xét.
+        style_loss = K.variable(value = 0.0)
+        weight = 1.0 /len(self.S["style_layers"])
+
+        # Lặp qua các lớp phong cách
+        for layer in self.S['style_layers']:
+            # Nắm bắt phong cách hiện tại và sử dụng chúng để trích xuất 
+            # các đặc trưng về phong cách và đầu ra.
+            style_output = layer_map[layer]
+            style_features = style_output[1, :, :, :]
+            output_features = style_features[2, :, :, :]
+
+            # Tính toán lỗi "đạo nháy" phong cách
+            T = self.style_recon_loss(
+                style_features,
+                output_features
+            )
+            style_loss += (weight * T)
+
+        # Tính hàm lỗi phong cách
+        style_loss *= self.S["style_weight"]
+        total_variational_loss = self.S["tv_weight"] * self.total_variational_loss(self.output)
+        total_loss = content_loss + style_loss + total_variational_loss
+
+        # Tính toán đạo hàm của đầu ra và hàm lỗi
+        grads = K.gradients(
+            loss= total_loss,
+            variables= self.output
+        )
+
+        outputs = [total_loss]
+        outputs += grads
+
+        # Tính độ lỗi và đạo hàm bằng phương pháp L-BFGS
+        self.loss_and_grads = K.function(
+            inputs= [self.output],
+            outputs= outputs
+        )
+
+
+
+    def preprocess(
+        self,
+
+    )-> tf.Tensor:
+        """_summary_
+
+        Returns:
+            Tensor: _description_
+        """
+        img = load_img()
+
+    def feauture_reconstruction(
+        self,
+
+    )-> Tensor:
+        """_summary_
+
+        Returns:
+            Tensor: _description_
+        """
+        pass
+
+    def style_recon_loss(
+        self,
+    )-> Tensor:
+        """_summary_
+
+        Returns:
+            Tensor: _description_
+        """
+        pass
+
+    def total_variational_loss(
+        self,
+    )-> Tensor:
+        """_summary_
+
+        Returns:
+            Tensor: _description_
+        """
+        pass
 
 
 
